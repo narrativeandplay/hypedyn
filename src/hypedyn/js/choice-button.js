464ead21cc88a945fedc0537352a03a8f90a1f14
Teong Leong <teongleong@gmail.com>
2012-04-10 17:01:34 +0800

    added copyright headers to the js code

--- src/hypedyn/js/choice-button.js
+++ src/hypedyn/js/choice-button.js
@@ -1,4 +1,24 @@
-var button_len = 250;
+/* Part of the HypeDyn project - http://www.partechgroup.org/hypedyn
+ * 
+ * Copyright (C) 2008-2011
+ * National University of Singapore
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
+ var button_len = 250;
 var button_height = "50px";
 var button_side_len = 50;
 
--- src/hypedyn/js/rules.js
+++ src/hypedyn/js/rules.js
@@ -1,3 +1,23 @@
+/* Part of the HypeDyn project - http://www.partechgroup.org/hypedyn
+ * 
+ * Copyright (C) 2008-2011
+ * National University of Singapore
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
 // links can have follow link (to node), change of text and update facts
 // nodes can have update facts
 // NOTE: special case, links in anywhere nodes cannot have follow link action
