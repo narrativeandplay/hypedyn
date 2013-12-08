d161217c5911e779a1a89da5d19de616ac9b17e9
Alex Mitchell <alexm@nus.edu.sg>
2013-12-08 12:53:28 +0800

    update rev numbers

--- src/hypedyn/about-hypedyn.scm
+++ src/hypedyn/about-hypedyn.scm
@@ -50,7 +50,7 @@
   (set-text-component about-editor #f #t)
   (set-text about-editor 
             "HypeDyn hypertext fiction editor 
-Version 2.3b rev 326, 15 August 2013\n
+Version 2.3b rev 333, 28 November 2013\n
 Copyright (C) 2008-2013
 National Univeristy of Singapore\n
 This program is free software; you can redistribute it and/or 
